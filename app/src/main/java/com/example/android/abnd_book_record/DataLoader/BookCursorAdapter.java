d028c8577aacacb892aa1e2dad3d7b501e9c2b80
Akanksha Rajwar <rajwar67@gmail.com>
2018-11-27 16:52:32 +0530

    added listener

--- app/src/main/java/com/example/android/abnd_book_record/DataLoader/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/abnd_book_record/DataLoader/BookCursorAdapter.java
@@ -30,6 +30,7 @@ import static java.security.AccessController.getContext;
 public class BookCursorAdapter extends CursorAdapter {
     int pos=-1;
 
+    TextView tvQuantity;
     BookProvider bookProvider;
     ContentValues contentValues;
     Uri currentUri;
@@ -52,7 +53,7 @@ public class BookCursorAdapter extends CursorAdapter {
     public void bindView(View view, Context context, final Cursor cursor) {
 
         TextView tvName=view.findViewById(R.id.name);
-        final TextView tvQuantity=view.findViewById(R.id.summary);
+        tvQuantity=view.findViewById(R.id.summary);
         TextView tvPrice=view.findViewById(R.id.price);
         Button buyButton=view.findViewById(R.id.buyButton);
 
@@ -61,27 +62,16 @@ public class BookCursorAdapter extends CursorAdapter {
         Integer priceIndex=cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRICE);
 
         tvName.setText(cursor.getString(nameIndex));
-        tvQuantity.setText("Stock : "+cursor.getString(quantityIndex));
+        tvQuantity.setText(cursor.getString(quantityIndex));
         tvPrice.setText("Rs. "+cursor.getString(priceIndex));
 
         View.OnClickListener myButtonClickListener = new View.OnClickListener() {
           @Override
             public void onClick(View v) {
 
-                String[] projection={
-                        BookContract.BookEntry.COLUMN_SELLER_NAME,
-                        BookContract.BookEntry.COLUMN_PRICE,
-                        BookContract.BookEntry.COLUMN_QUANTITY
-                };
-              currentUri=cursor.getNotificationUri();
-              //String selection= BookContract.BookEntry._ID+"=?";
-             //String[]  selectionArgs=new String[]{String.valueOf(ContentUris.parseId(currentUri))};
-               contentValues=new ContentValues();
-               int quantity=Integer.parseInt(cursor.getString(quantityIndex));
-              contentValues.put(BookContract.BookEntry.COLUMN_QUANTITY, quantity-1);
+               int quantity=Integer.parseInt(tvQuantity.getText().toString());
 
-              int rowsAffected =bookProvider.update(currentUri, contentValues, null, null);
-              tvQuantity.setText("Stock : "+cursor.getString(quantityIndex));
+              tvQuantity.setText(quantity-1);
 
             }
         };
